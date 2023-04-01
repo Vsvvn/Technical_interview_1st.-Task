75047dc6cb4115a63cd32582b85fe92252cb9f23
Vsvvn <Vladsun@yandex.ru>
2023-04-02 01:48:37 +0300

    Задача решена.

--- src/main/java/task/example/Main.java
+++ src/main/java/task/example/Main.java
@@ -1,7 +1,34 @@
 package task.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<User> users = new ArrayList<>();
+
+        User irina = new User("Ирина", 28);
+        User irina1 = new User("Ирина", 28);
+        User ivan = new User("Иван", 21);
+        User petr = new User("Петр", 23);
+        User anna = new User("Анна", 22);
+
+        users.add(irina);
+        users.add(irina1);
+        users.add(ivan);
+        users.add(petr);
+        users.add(anna);
+
+        System.out.println(getMaxAgeUser(users));
+    }
+
+    private static Optional<User> getMaxAgeUser(Collection<User> collection) {
+        return collection.stream()
+                .distinct()
+                .sorted(Comparator.comparingInt(User::getAge).thenComparing(User::getName))
+                .max(Comparator.comparingInt(User::getAge));
     }
 }
